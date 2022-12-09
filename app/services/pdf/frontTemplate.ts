4924d52fb0898924cc671e4136adda5ce8c40ffc
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2022-12-09 15:44:34 +0300

    to update

--- app/services/pdf/frontTemplate.ts
+++ app/services/pdf/frontTemplate.ts
@@ -238,7 +238,7 @@ export const generateFrontTemplate = () => {
         ><span class="under"> ${student.grade}</span>
       </span>
       <span style="display: inline-block; font-weight: bold; font-size:0.85em;margin-right: 15px;">
-      ____________to<span class="under"  style="display: inline-block; font-weight: bold;margin: 0px 10px;"></span>
+      <span class="under"  style="display: inline-block; font-weight: bold;margin: 0px 10px;"></span>
       </span>
     </div>
   </div>
